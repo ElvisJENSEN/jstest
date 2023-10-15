4a9388b07023ce1ee919389d7672bb48a8345e51
ElvisJENSEN <103517327+ElvisJENSEN@users.noreply.github.com>
2023-10-15 18:13:00 +0800

    Update watson.js

--- watson.js
+++ watson.js
@@ -1,1 +1,17 @@
-console.log('---------------------------------------------')
+var body = $response.body;
+//var obj = JSON.parse(body);
+
+//obj['data']['isMember'] = 1;
+//obj['data']['expireTime'] = "2023-07-12";
+// body = JSON.stringify(obj);
+$httpClient.get("http://192.168.10.264:20480/api/test", function (error, response, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+            }
+        });
+console.log(body);
+//var body2 = $request.headers;
+
+$done(body);
